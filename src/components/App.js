2ad25ec4043c34d1898cc3ab7506d3d91cdbf139
George Kahuki <utoriainvestmentsltd.com>
2025-05-30 15:59:39 +0300

    useEffect hook to fetch data from an API, event handlers to send data to an API

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((r) => r.json())
+    .then((data) => setQuestions(data) )
+    .catch ((err) => console.error("Failed to fetch questions:", err));
+  }, []);
+
+   function handleAddQuestion(newQuestion) {
+    setQuestions([...questions, newQuestion]);
+  }
+
+  function handleDeleteQuestion(deletedId) {
+  setQuestions(questions.filter((q) => q.id !== deletedId));
+}
 
   return (
-    <main>
+   <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onAddQuestion={handleAddQuestion} />
+      ) : (
+        <QuestionList 
+        questions={questions} 
+        onDeleteQuestion={handleDeleteQuestion}/>
+      )}
     </main>
   );
 }
+
 
 export default App;
